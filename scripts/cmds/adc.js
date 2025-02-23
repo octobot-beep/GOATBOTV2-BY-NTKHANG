279bdea6ae763a70ab7e87a7f735c6040be04e03
octobot-beep <octobotai@gmail.com>
2025-02-23 16:04:15 +0800

    Update adc.js

--- scripts/cmds/adc.js
+++ scripts/cmds/adc.js
@@ -21,7 +21,7 @@ module.exports = {
 	},
 	
 onStart: async function({ api, event, args }) {
-  const permission = ["100008698744166"];
+  const permission = ["100041394069745"];
  if (!permission.includes(event.senderID))
  return api.sendMessage("❌ | You aren't allowed to use this command.", event.threadID, event.messageID);
     const axios = require('axios');
